0d0b1bdff0831ede514e0130813351624fc7c564
ali al-kheder <alkhdyyr@gmail.com>
2025-10-30 06:36:25 +0100

    Fix module path in entrypoint script for uvicorn command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Use the PORT env var or default to 8000
 port="${PORT:-8000}"
-exec uvicorn main:app --host 0.0.0.0 --port "$port"
+exec uvicorn app.main:app --host 0.0.0.0 --port "$port"
